a518a0654e3f120a890796f15c9f462b2f6a1c3c
Livio Beqiri <liviob@live.com>
2023-11-08 15:58:09 -0500

    Introducing Data Frames....

--- working-with-an-excel-sheet.R
+++ working-with-an-excel-sheet.R
@@ -3,4 +3,15 @@ library(readxl)
 
 #Get Excel File Data - Excel_Data.xlsx
 excel_data <- read_xlsx("C:/Users/LivioBeqiri/Desktop/R/excel_data.xlsx", sheet = "Sheet1")
-print(excel_data)
+
+#Define Excel Path
+excel_path <- "C:/Users/LivioBeqiri/Desktop/R/excel_data.xlsx"
+
+#Get Excel Columns and store them into a variable
+title <- read.xlsx(excel_path, sheetName = "Sheet1", colIndex = 1)
+price <- read.xlsx(excel_path, sheetName = "Sheet1", colIndex = 2)
+
+#Create a data-Frame for all excel columns
+DataFrame <- data.frame(title,price)
+
+print(DataFrame)
