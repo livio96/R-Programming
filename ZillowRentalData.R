92cec29890c81cfe309099b1195bf62128baaa7a
Livio Beqiri <liviob@live.com>
2023-11-28 20:15:49 -0500

    Update ZillowRentalData.R

--- ZillowRentalData.R
+++ ZillowRentalData.R
@@ -10,4 +10,27 @@ raw_data <- read_excel(path)
 newark_nj <- filter(raw_data, State == 'NJ' & RegionName == 'Newark' )
 subsetted_newark_nj = subset(newark_nj, select = -c(RegionID,SizeRank,RegionName,RegionType,StateName,State, Metro, CountyName) )
 print(subsetted_newark_nj)
-barplot(as.matrix(subsetted_newark_nj), ylim = c(0,2000))
+#barplot(as.matrix(subsetted_newark_nj), ylim = c(0,2000))
+
+library(shiny)
+library(shinythemes)
+library(ggplot2)
+library(readxl)
+library(xlsx)
+library(dplyr)
+library(tidyverse)
+library(raster)
+
+
+
+file_path <- '/Users/liviobeqiri/Documents/R/R-Programming/Revenue.xlsx'
+raw_data <- read_excel(file_path)
+data_df <- as.data.frame(raw_data)
+data_filtered_by_revenue <- filter(data_df, Revenue>200000)
+
+
+
+# Barplot
+ggplot(data_filtered_by_revenue, aes(x=Brand, y=Revenue)) + 
+  geom_bar(stat = "identity") + coord_cartesian(ylim = c(500000, 5000000))
+
