2161e2f7fc14ffdbf05b672db2343571ea90779e
Livio Beqiri <liviob@live.com>
2024-01-06 22:04:26 -0500

    Added one more report

--- InventoryAudit.R
+++ InventoryAudit.R
@@ -1,33 +1,36 @@
 library(xlsx)
 library(readxl)
-library(tidyverse)
 library(dplyr)
+library(tidyverse)
 
-path = '/Users/liviobeqiri/Documents/R/R-Programming/SerializedInventory.xlsx'
-raw_data <- read_excel(path)
-
+path = '/cloud/project/R/InventoryDetailOnHandBinStatusResults.csv'
+raw_data <- read.csv(path)
+print(raw_data)
 serials <- raw_data[2] # Get Serial Number column
-only_serialized_inventory <- filter(raw_data, `Inventory Number` != '') #Remove non-serialized rows
-
+only_serialized_inventory <- filter(raw_data, `Inventory.Number` != '') #Remove non-serialized rows
 
 #_______ Start of Duplicate Detection Code _______
 
 duplicated_serial_numbers <- only_serialized_inventory[duplicated(only_serialized_inventory$`Inventory Number`) ,] #Get All Duplicated Rows - where serial number is duplicated
-merged_data <- merge(raw_data, duplicated_serial_numbers, by.x = 'Inventory Number', by.y = 'Inventory Number')
-write.csv(merged_data, '/Users/liviobeqiri/Documents/R/R-Programming/DuplicatedSerials.csv') #create a csv file with all duplicated serials
+merged_data <- merge(raw_data, duplicated_serial_numbers, by.x = 'Inventory.Number', by.y = 'Inventory.Number')
+write.csv(merged_data, '/cloud/project/R/DuplicatedSerials.csv') #create a csv file with all duplicated serials
 
 #_______ End of Dulicate Detection Code _________
-
 
 #_______ Start of SN Status Check Code ___________
 
 only_serialized_inventory_df <- data.frame(only_serialized_inventory)
 sn_in_good_status <- filter(only_serialized_inventory_df, Status == 'Good') #Pull a list of all sn that are in good status
-sn_in_good_status_wrong_bin <- filter(sn_in_good_status, (Bin.Number == 'A-RTV-01' | Bin.Number == 'Trash' | Bin.Number == 'StockCount1' | Bin.Number == 'Not Counted', | Bin.Number == 'Refurbishing', | Bin.Number == 'Testing'))
+sn_in_good_status_wrong_bin <- filter(sn_in_good_status, (Bin.Number == 'A-RTV-01' | Bin.Number == 'Trash' | Bin.Number == 'StockCount1' | Bin.Number == 'Not Counted' | Bin.Number == 'Refurbishing' | Bin.Number == 'Testing' | Bin.Number == 'Amazon Removal' | Bin.Number == 'Parts'))
 print(sn_in_good_status_wrong_bin)
-write.csv(sn_in_good_status_wrong_bin, '/Users/liviobeqiri/Documents/R/R-Programming/GoodStatusWrongBin.csv') #create a csv file with all duplicated serials
+write.csv(sn_in_good_status_wrong_bin, '/cloud/project/R/GoodStatusWrongBin.csv') #create a csv file with all duplicated serials
 
+#______ End of SN Status Check code _______
 
+sn_not_in_good_status <- filter(only_serialized_inventory_df, Status != 'Good')
+sn_not_in_good_status_cd_bin <- filter(sn_not_in_good_status, (startsWith(Bin.Number, "CD-")))
+print(sn_not_in_good_status_cd_bin)
+write.csv(sn_not_in_good_status_cd_bin, '/cloud/project/R/NotGoodStatusCDBin.csv')
 
 
 
