f28116a6ad0b1d9030283c57038331a8d6731ec6
Livio Beqiri <liviob@live.com>
2024-02-12 09:49:28 -0500

    Update TotalPOProfitability.R

--- Purchase Order Profitability/TotalPOProfitability.R
+++ Purchase Order Profitability/TotalPOProfitability.R
@@ -4,10 +4,12 @@ library(dplyr)
 library(readr)
 
 #Pull data from datasets
-Received <- read_csv("/Users/liviobeqiri/Documents/R/Purchase Order Profitability/Received.csv")
-Inventory <- read_csv("/Users/liviobeqiri/Documents/R/Purchase Order Profitability/Inventory.csv")
-Fulfilled <- read_csv("/Users/liviobeqiri/Documents/R/Purchase Order Profitability/Fulfilled.csv")
-ReceivedAndSold <- read_csv("/Users/liviobeqiri/Documents/R/Purchase Order Profitability/ReceivedAndSold.csv")
+#Received <- read_csv("/Users/liviobeqiri/Documents/R/Purchase Order Profitability/Received.csv")
+
+ReceivedAndSold <- read_csv("C:/Users/LivioBeqiri/Desktop/R/PO Profitability/ReceivedAndSold.csv")
+Fulfilled <- read_csv("C:/Users/LivioBeqiri/Desktop/R/PO Profitability/Fulfilled.csv")
+Received <- read_csv("C:/Users/LivioBeqiri/Desktop/R/PO Profitability/Received.csv")
+Inventory <- read_csv("C:/Users/LivioBeqiri/Desktop/R/PO Profitability/Inventory.csv")
 
 
 merged_data <- merge(Received, Fulfilled, by="Serial Number")
@@ -17,14 +19,31 @@ merged_data <- transform(
   merged_data, profit = ifelse(merged_data$`Item Rate`!= '' & merged_data$Rate != '', merged_data$Rate - merged_data$`Item Rate` - merged_data$Fees)
 )
 
-
-merged_data <- merged_data %>% 
+#Sales Rep Summary
+merged_data_rep <- merged_data %>% 
   group_by(Sales.Rep) %>% 
   summarise(Total_Profit = sum(profit), Revenue = sum(Rate), Cost = sum(Item.Rate), Fees = sum(Fees))
 
-view(merged_data)
+#Item Summary
+merged_data_item <- merged_data %>% 
+  group_by(Name) %>% 
+  summarise(Total_Profit = sum(profit), Revenue = sum(Rate), Cost = sum(Item.Rate), Fees = sum(Fees))
 
-  
+#Vendor Summary
+merged_data_vendor <- merged_data %>% 
+  group_by(Main.Line.Name) %>% 
+  summarise(Total_Profit = sum(profit), Revenue = sum(Rate), Cost = sum(Item.Rate), Fees = sum(Fees))
+
+#PO Summary
+merged_data_po <- merged_data %>% 
+  group_by(Document.Number) %>% 
+  summarise(Total_Profit = sum(profit), Revenue = sum(Rate), Cost = sum(Item.Rate), Fees = sum(Fees))
+
+
+write.xlsx(merged_data_rep, file="C:/Users/LivioBeqiri/Desktop/R/PO Profitability/TotalPOProfitability.xlsx", sheetName = 'PO Profitability - Rep')
+write.xlsx(merged_data_item, file="C:/Users/LivioBeqiri/Desktop/R/PO Profitability/TotalPOProfitability.xlsx", sheetName = 'PO Profitability - Item', append = TRUE)
+write.xlsx(merged_data_vendor, file="C:/Users/LivioBeqiri/Desktop/R/PO Profitability/TotalPOProfitability.xlsx", sheetName = 'PO Profitability - Vendor', append = TRUE)
+write.xlsx(merged_data_po, file="C:/Users/LivioBeqiri/Desktop/R/PO Profitability/TotalPOProfitability.xlsx", sheetName = 'PO Profitability - PO', append = TRUE)
 
 
 
